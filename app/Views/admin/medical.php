98c2471f5c8e91a469767b872ae6c8d8a33d4d89
AbielB <abielbahyria.offical@gmail.com>
2023-01-06 16:54:48 +0700

    medical

--- app/Views/admin/medical.php
+++ app/Views/admin/medical.php
@@ -6,10 +6,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Link Favicon -->
-    <link rel="shortcut icon" href="./img/logo/pearlmedic.png" type="image/x-icon">
+    <link rel="shortcut icon" href="<?php echo base_url("img/logo/pearlmedic.png"); ?>" type="image/x-icon">
     <!-- END Link Favicon -->
-    <!-- Link CSS -->
-    <link rel="stylesheet" href="./css/style.min.css?v=<?php echo time(); ?>">
+    <!-- CSS Links -->
+    <link rel="stylesheet" href=" <?php echo base_url("css/style.min.css?v=" . time()); ?>">
     <!-- END Link CSS -->
     <!-- Box Icons -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
@@ -22,51 +22,48 @@
 
 <body>
     <!-- ADMIN MENU SECTION -->
-    <div class="container-2">
+    <div class="container">
         <div class="adminmenu">
             <div class="top">
                 <div class="logo">
-                    <img src="./img/logo/pearlmedic2.png" alt="" width="10%">
-                    <h2><span>Pearl</span> Medic</h2>
+                    <img src="../img/logo/pearlmedic2.png" alt="" width="10%">
+                    <h2 id="color-change"><span>Pearl</span> Medic</h2>
                 </div>
                 <div class="close" id="close-btn">
                     <i class="fa-solid fa-xmark"></i>
                 </div>
             </div>
             <div class="sidebar">
-                <a href="index.php" class="paragraph-change">
-                    <i class="fa-solid fa-house"></i>
-                    <h3>Dashboard</h3>
-                </a>
-                <a href="medical.php" class="active">
-                    <i class="fa-solid fa-notes-medical" id="span"></i>
-                    <h3>Layanan Medical</h3>
-                </a>
-                <a href="obat.php">
-                    <i class="fa-solid fa-capsules"></i>
-                    <h3>Layanan Obat</h3>
-                </a>
-                <a href="darurat.php">
-                    <i class="fa-solid fa-truck-medical"></i>
-                    <h3>Layanan Darurat</h3>
-                </a>
-                <a href="vaksin.php">
-                    <i class="fa-solid fa-syringe"></i>
-                    <h3>Layanan Vaksin</h3>
-                </a>
-                <a href="dataperusahaan.php">
-                    <i class="fa-solid fa-city"></i>
-                    <h3>Data Perusahaan</h3>
-                </a>
-                <!-- <a href="pesan.php">
-                    <i class="fa-solid fa-envelope"></i>
-                    <h3>Pesan</h3>
-                    <p class="message-count">12</p>
-                </a> -->
-                <a href="login.php">
-                    <i class="fa-solid fa-right-from-bracket"></i>
-                    <h3>Logout</h3>
-                </a>
+                <div class="sidebar">
+                    <a href="/admin">
+                        <i class="fa-solid fa-house" id="span"></i>
+                        <h3>Dashboard</h3>
+                    </a>
+                    <a href="/admin/LayananMedical" class="active">
+                        <i class="fa-solid fa-notes-medical"></i>
+                        <h3>Layanan Medical</h3>
+                    </a>
+                    <a href="/admin/LayananObat" id="paragraph-change">
+                        <i class="fa-solid fa-capsules"></i>
+                        <h3>Layanan Obat</h3>
+                    </a>
+                    <a href="/admin/LayananDarurat">
+                        <i class="fa-solid fa-truck-medical"></i>
+                        <h3>Layanan Darurat</h3>
+                    </a>
+                    <a href="/admin/LayananVaksin">
+                        <i class="fa-solid fa-syringe"></i>
+                        <h3>Layanan Vaksin</h3>
+                    </a>
+                    <a href="/admin/DataPerusahaan">
+                        <i class="fa-solid fa-city"></i>
+                        <h3>Data Perusahaan</h3>
+                    </a>
+                    <a href="/admin/Logout">
+                        <i class="fa-solid fa-right-from-bracket"></i>
+                        <h3>Logout</h3>
+                    </a>
+                </div>
             </div>
         </div>
 
@@ -77,6 +74,61 @@
             <div class="date">
                 <input type="date" name="date" id="date">
             </div>
+
+            <div class="orderterbaru">
+                <h2>Pesanan Medical Check Up Terbaru </h2>
+                <form method="get">
+                    <div class="search">
+                        <input type="text" name="search" id="search" placeholder="Cari Nama Perusahaan....">
+                        <button type="submit" name="submit" id="submit"><i class="fa-solid fa-search"></i></button>
+                    </div>
+                </form>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Nama Perusahaan</th>
+                            <th>Lokasi</th>
+                            <th>Jumlah Orang</th>
+                            <th>Status</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        foreach ($rowMedical as $m) {
+                            if ($m->status == 1) {
+                                $color = "red";
+                            } else if ($m->status == 2) {
+                                $color = "yellow";
+                            } else {
+                                $color = "green";
+                            }
+                        ?>
+                        <tr>
+                            <td><?= $m->nama_perusahaan; ?></td>
+                            <td><?= $m->lokasi; ?></td>
+                            <td><?= $m->jumlah; ?></td>
+                            <!-- select status-->
+                            <td>
+                                <select name="status" id="status" class="status"
+                                    onchange="statusMedical(<?php echo $m->id_checkup; ?>, this.value)">
+                                    <option value=" 1" <?php if ($m->status == 1) {
+                                                                echo "selected";
+                                                            } ?>>Belum Dilayani</option>
+                                    <option value="2" <?php if ($m->status == 2) {
+                                                                echo "selected";
+                                                            } ?>>Dalam Proses</option>
+                                    <option value="3" <?php if ($m->status == 3) {
+                                                                echo "selected";
+                                                            } ?>>Sudah Dilayani</option>
+                                </select>
+                        </tr>
+                        <?php } ?>
+
+                    </tbody>
+
+                </table>
+            </div>
         </main>
         <!-- END MAIN MENU SECTION -->
         <!-- BAGIAN KANAN MAIN MENU / DETAILS MENU -->
@@ -86,16 +138,13 @@
                     <i class="fa-solid fa-bars"></i>
                 </button>
                 <!-- <div class="theme-toggler">
-                    <i class="fa-solid fa-sun active"></i>
-                    <i class="fa-solid fa-moon"></i>
+                    <i class="fa-solid fa-sun active" id="white-btn"></i>
+                    <i class="fa-solid fa-moon" id="black-btn"></i>
                 </div> -->
                 <div class="profile">
                     <div class="info">
-                        <p>Hallo, <span>Hanif</span> </p>
+                        <p>Hallo, <span><?php echo $name ?></span> </p>
                         <p class="text-muted">Admin</p>
-                    </div>
-                    <div class="profile-photo">
-                        <img src="./img/assets/fotocandid3.png" alt="">
                     </div>
                 </div>
             </div>
